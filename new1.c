b5ff36107382094ed1f926a3bd3f39522c9fe23f
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-01-24 16:35:59 +0530

    Create new1.c

---
+++ new1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+void main()
+{
+    float n;
+    clrscr();
+
+    printf("Enter the number:-");
+    scanf("%f",&n);
+
+    printf("The integer number after rounded is  %5.0f",n);
+    getch();
+}
