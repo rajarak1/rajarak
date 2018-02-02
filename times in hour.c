1b9c8088e90a8debaee1f8328db349359ff188bf
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-02-02 15:47:58 +0530

    Create times in hour.c

---
+++ times in hour.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+int a,b,c,d;
+float i;
+scanf("%d%d%d%d",&a,&b,&c,&d);
+c=a-b;
+g=e*60;
+f=c-d;
+h=g+f;
+if(h>=60)
+{
+	i=h/60;
+printf("%d hour %d minute",c,i);
+}
+else
+{
+	printf("%dminutes",h);
+}
+	return 0;
+}
